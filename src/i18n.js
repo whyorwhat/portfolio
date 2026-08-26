4ed1d97936bca168bc2f786c2354c34f888547c5
Simone Cotardo <simo.cotardo@gmail.com>
2026-08-26 10:15:35 +0200

    renamed locales for replyia

--- src/i18n.js
+++ src/i18n.js
@@ -1,8 +1,8 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
-import en from './locales/en/whatsapp.json';
-import it from './locales/it/whatsapp.json';
+import en from './locales/en/replyia.json';
+import it from './locales/it/replyia.json';
 
 i18n
     .use(initReactI18next)
